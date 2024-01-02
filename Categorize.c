1edb09aeec827e61f94fd03dc871db349d0cfa26
kingshalem <147488963+kingshalem@users.noreply.github.com>
2024-01-02 15:27:38 +0530

    #include<stdio.h> int main() { float h; scanf("%f",&h); if(h<150.0) printf("The person is Dwarf."); else if(h>=150.0&&h<=165.0) printf("The person is average heighted."); else if(h>165.0&&h<195.0) printf("The person is taller."); else if(h>195.0) printf("Abnormal height."); }

---
+++ Categorize.c
@@ -0,0 +1,2 @@
+#include<stdio.h>
+int main() { float h; scanf("%f",&h); if(h<150.0) printf("The person is Dwarf."); else if(h>=150.0&&h<=165.0) printf("The person is average heighted."); else if(h>165.0&&h<195.0) printf("The person is taller."); else if(h>195.0) printf("Abnormal height."); }
